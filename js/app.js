2c85dee58041a2bfa46c500ad5179cd6427f660e
Matt Rudge <matt.rudge@codeinstitute.net>
2018-01-24 21:00:33 +0000

    Routing not working

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ angular.module("dessertsApp", ["ngRoute", "RouteControllers"]);
 angular.module("dessertsApp").config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);
  
-    $routeProvider.when(".", {
+    $routeProvider.when("./", {
         templateUrl: "templates/home.html"
     })
     .when("add", {
